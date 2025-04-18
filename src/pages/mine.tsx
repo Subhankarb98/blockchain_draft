3bbcc37739a36f008016c0c8400837383317fdd7
Subhankar Bhattacharjee <112893286+Subhankarb98@users.noreply.github.com>
2025-04-18 14:06:44 +0530

    Delete src directory

--- src/pages/mine.tsx
+++
@@ -1,9 +0,0 @@
-function Mine () {
-    return (
-        <div className="mx-2 my-2">
-            <h3>🚧 Mining page. 🛠</h3>
-        </div>
-    );
-};
-
-export default Mine;
--- src/pages/transact.tsx
+++
@@ -1,9 +0,0 @@
-function Transact () {
-    return (
-        <div className="mx-2 my-2">
-            <h3>🚧 This is where you can add transactions. 🛠</h3>
-        </div>
-    );
-};
-
-export default Transact;
